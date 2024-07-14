6df4b630a3cac9e8928104185cfde57a28613a5f
unknown <envizajmi@gmail.com>
2024-07-14 17:41:33 +0200

    Header Compoennt Edired

---
+++ FrontEnd/ReactProject/clothingstore/src/Components/Header.jsx
@@ -0,0 +1,24 @@
+function Header(){
+
+    return (
+
+        <div>
+
+
+        <header>Lets Start</header>
+        <h2>This is a testing work</h2>
+        <p>Yes</p>
+
+
+        </div>
+
+
+
+    );
+
+
+
+}
+
+export default Header
+
